8edbe275df7c6d39521f8a772c8b4bd4f705dd4e
greene <green@DESKTOP-9R3UJ0N>
2022-10-12 07:25:19 -0400

    Created User Input class, created methods for ID, Start Time and End Time.

---
+++ CodingTracker/UserInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker
+{
+    internal class UserInput
+    {
+        internal static int GetCodingEntryId()
+        {
+            Console.WriteLine(@"Please enter the Id of the record in the table below);
+        }
+        internal static string GetStartTime()
+        {
+            Console.WriteLine(@"Please enter a date and time in the following format: MM/DD/YYYY 23:59");
+            string StartTime = Console.ReadLine();
+            return StartTime;
+        }
+
+        internal static string GetEndTime()
+        {
+            Console.WriteLine(@"Please enter a date and time in the following format: MM/DD/YYYY 23:59");
+            string EndTime = Console.ReadLine();
+            return EndTime;
+        }
+    }
+}

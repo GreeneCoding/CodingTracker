d7572f3b3521fcef4ef3522b8719999ad7d7feb8
greene <green@DESKTOP-9R3UJ0N>
2022-10-16 17:09:28 -0400

    Added User Validation class, started the Id, StartTime, Endtime Validations

---
+++ CodingTracker/UserValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker
+{
+    internal class UserValidation
+    {
+        internal static int IdValidation(int id, List<CodingSession> codingsessionsdata)
+        {
+            bool idexists = codingsessionsdata.Exists(x=> x.Id == id);
+            if (id == 0) UserMenu.ShowUserMenu();
+            while (idexists == false)
+            {
+                Console.WriteLine("Invalid id entered, please try again.");
+                UserInput.GetCodingEntryId();
+            }
+            return id;
+        }
+        internal static string StartTimeValidation(string starttime)
+        {
+            return starttime;
+        }
+        internal static string EndTimeValidation(string endtime)
+        {
+            return endtime;
+        }
+    }
+}
